d15b7bdd27681489f3cf1a7c48b31271660b4134
TriciaF <TriciaF@thinkful.com>
2017-12-07 16:31:56 -0500

    Completed ShoppingList

--- shoppingList/index.js
+++ shoppingList/index.js
@@ -8,26 +8,31 @@ delete an item:  remove <li> event listener on delete
 
 function addItem() {
 
-  $('#js-shopping-list-form').on('submit', event => {
-    event.preventDefault();
+    $('#js-shopping-list-form').on('submit', event => {
+        event.preventDefault();
 
-    let itemName = $('.js-shopping-list-entry').val();
-    console.log(`${itemName}`);
+        let itemName = $('.js-shopping-list-entry').val();
+        console.log(`${itemName}`);
 
-    $('.shopping-list').append(`<li><span class="shopping-item">${itemName}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
+        $('.shopping-list').append(`<li><span class="shopping-item">${itemName}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
 
-  });
+    });
 
 }
 
-
-
 function checkItem() {
-  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-  });
+    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+    });
+}
+
+function deleteItem() {
+    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+        $(this).closest('li').remove();
+    });
 }
 
 
 $(addItem);
 $(checkItem);
+$(deleteItem);
