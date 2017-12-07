28235a4683f1631c6309d56fc3fbbbaf3189bea1
David Park <dysp92@gmail.com>
2017-12-07 11:33:04 -0800

    finished fizzbuzz

--- FizzBuzz/index.js
+++ FizzBuzz/index.js
@@ -1,27 +1,29 @@
+'use strict'
+
 function fizzBuzz() {
 
-    $('#number-chooser').on('submit', event => {
+  $('#number-chooser').on('submit', event => {
 
-        event.preventDefault();
+    event.preventDefault();
 
-        const numEntered = parseInt($('#number-choice').val());
-        console.log(numEntered);
-        for (let i = 1; i <= numEntered; i++) {
-            if (i % 15 === 0) {
-                console.log(15);
-                $('.js-result').append('<div class="fizz-buzz-item fizzbuzz"></div>');
-                $('.fizz-buzz-item').append("<span>fizzbuzz</span>");
-            } else if (i % 5 === 0) {
-                console.log(5);
-                $('.js-result').append('<div class="fizz-buzz-item buzz"><span>"buzz"</span></div>');
-            } else if (i % 3 === 0) {
-                console.log(3);
-                $('.js-result').append('<div class="fizz-buzz-item fizz"><span>"fizz"</span></div>');
-            } else {
-                console.log(0);
-                $('.js-result').append(`<div class="fizz-buzz-item"><span>${i}</span></div>`);
-            }
-        }
-    });
+    const numEntered = parseInt($('#number-choice').val());
+    console.log(numEntered);
+    for (let i = 1; i <= numEntered; i++) {
+      if (i % 15 === 0) {
+        console.log(15);
+        $('.js-results').append('<div class="fizz-buzz-item fizzbuzz"><span>fizzbuzz</span></div>');
+        // $('.fizz-buzz-item').append('');
+      } else if (i % 5 === 0) {
+        console.log(5);
+        $('.js-results').append('<div class="fizz-buzz-item buzz"><span>buzz</span></div>');
+      } else if (i % 3 === 0) {
+        console.log(3);
+        $('.js-results').append('<div class="fizz-buzz-item fizz"><span>fizz</span></div>');
+      } else {
+        console.log(0);
+        $('.js-results').append(`<div class="fizz-buzz-item"><span>${i}</span></div>`);
+      }
+    }
+  });
 }
 $(fizzBuzz);
