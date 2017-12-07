08481b5952e0fd61e631e1deb32fbd548820e755
TriciaF <TriciaF@thinkful.com>
2017-12-07 15:12:59 -0500

    no change

--- FizzBuzz/index.js
+++ FizzBuzz/index.js
@@ -5,21 +5,22 @@ function fizzBuzz() {
         event.preventDefault();
 
         const numEntered = parseInt($('#number-choice').val());
+
         console.log(numEntered);
+
         for (let i = 1; i <= numEntered; i++) {
             if (i % 15 === 0) {
                 console.log(15);
-                $('.js-result').append('<div class="fizz-buzz-item fizzbuzz"></div>');
-                $('.fizz-buzz-item').append("<span>fizzbuzz</span>");
+                $('.js-results').append("<div class=fizz-buzz-item fizzbuzz><span>fizzbuzz</span></div>");
             } else if (i % 5 === 0) {
                 console.log(5);
-                $('.js-result').append('<div class="fizz-buzz-item buzz"><span>"buzz"</span></div>');
+                $('.js-results').append("<div class=fizz-buzz-item buzz><span>buzz</span></div>");
             } else if (i % 3 === 0) {
                 console.log(3);
-                $('.js-result').append('<div class="fizz-buzz-item fizz"><span>"fizz"</span></div>');
+                $('.js-results').append('<div class="fizz-buzz-item fizz"><span>"fizz"</span></div>');
             } else {
                 console.log(0);
-                $('.js-result').append(`<div class="fizz-buzz-item"><span>${i}</span></div>`);
+                $('.js-results').append(`<div class="fizz-buzz-item"><span>${i}</span></div>`);
             }
         }
     });
