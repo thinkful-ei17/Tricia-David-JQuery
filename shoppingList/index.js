f5dadf933ccda7b121a2e5e783f37c8e873e66af
TriciaF <TriciaF@thinkful.com>
2017-12-07 17:16:31 -0500

    modified settings.json and index.js files

--- shoppingList/index.js
+++ shoppingList/index.js
@@ -1,35 +1,35 @@
 'use strict';
 /*
 add item:  add new <li> have event listener on form
-check off item:  add new class shopping-item__checked event listener on check 
+check off item:  add new class shopping-item__checked event listener on check
 uncheck an item:  remove shopping-item__checked event lister on check
 delete an item:  remove <li> event listener on delete
 */
 
 function addItem() {
 
-    $('#js-shopping-list-form').on('submit', event => {
-        event.preventDefault();
+  $('#js-shopping-list-form').on('submit', event => {
+    event.preventDefault();
 
-        let itemName = $('.js-shopping-list-entry').val();
-        console.log(`${itemName}`);
+    let itemName = $('.js-shopping-list-entry').val();
+    console.log(`${itemName}`);
 
-        $('.shopping-list').append(`<li><span class="shopping-item">${itemName}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
+    $('.shopping-list').append(`<li><span class="shopping-item">${itemName}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
 
-    });
+  });
 
 }
 
 function checkItem() {
-    $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
-        $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
-    });
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
 }
 
 function deleteItem() {
-    $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
-        $(this).closest('li').remove();
-    });
+  $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+    $(this).closest('li').remove();
+  });
 }
 
 
