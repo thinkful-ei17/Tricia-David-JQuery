94e7359935830efc8e0eec7517fba5bb5727c38d
David Park <dysp92@gmail.com>
2017-12-07 13:18:19 -0800

    checkItem added

--- shoppingList/index.js
+++ shoppingList/index.js
@@ -1,3 +1,4 @@
+'use strict';
 /*
 add item:  add new <li> have event listener on form
 check off item:  add new class shopping-item__checked event listener on check 
@@ -7,16 +8,26 @@ delete an item:  remove <li> event listener on delete
 
 function addItem() {
 
-    $('#js-shopping-list-form').on('submit', event => {
-        event.preventDefault();
+  $('#js-shopping-list-form').on('submit', event => {
+    event.preventDefault();
 
-        let itemName = $('.js-shopping-list-entry').val();
-        console.log(`${itemName}`);
+    let itemName = $('.js-shopping-list-entry').val();
+    console.log(`${itemName}`);
 
-        $('.shopping-list').append(`<li><span class="shopping-item">${itemName}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`)
+    $('.shopping-list').append(`<li><span class="shopping-item">${itemName}</span><div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div></li>`);
 
-    })
+  });
 
 }
 
+
+
+function checkItem() {
+  $('.shopping-list').on('click', '.shopping-item-toggle', function(event) {
+    $(this).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+  });
+}
+
+
 $(addItem);
+$(checkItem);
